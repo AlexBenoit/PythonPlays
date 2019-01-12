5a7786bd3bd43ae253c42fb4a249539ef6317840
Alexandre <alexandre.benoit@live.com>
2019-01-11 19:08:36 -0500

    Added clarifications

--- PythonPlays/PythonPlays.py
+++ PythonPlays/PythonPlays.py
@@ -9,18 +9,22 @@ import grabScreen
 
 print("starting")
 
-WINDOW_WIDTH = 800
-WINDOW_HEIGHT = 600
+WINDOW_X = 1                                # Default image position for a window perfectly in top left corner
+WINDOW_Y = 38                               # Default image position for a window perfectly in top left corner
+WINDOW_WIDTH = 1280 - WINDOW_X * 2          # Modify these values for a window snapped in top left corner
+WINDOW_HEIGHT = 720 - WINDOW_Y - WINDOW_X   # Modify these values for a window snapped in top left corner
 
 def start_playing():
     last_time = time.time()
     while True:
         # windowed mode
-        screen =  grabScreen. grab_screen_RGB(region=(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT))
-        #new_screen, original_image, m1, m2 = imgProc.process_img(screen)
+        screen =  grabScreen. grab_screen_RGB(region=(WINDOW_X, WINDOW_Y, WINDOW_WIDTH, WINDOW_HEIGHT))
+        
+        # Image processing goes here if needed
 
-        #cv2.imshow("window", screen)
-        #cv2.imshow("window2", screen)
+        cv2.imshow("window", screen) # Window showing what is captured
+
+        # Decision making goes here
 
         if cv2.waitKey(25) & 0xFF == ord('q'):
             cv2.destroyAllWindows()
