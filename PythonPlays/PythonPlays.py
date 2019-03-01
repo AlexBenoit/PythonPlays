5afe19636510b1c3bcb02abb02bdc19b95e1a3da
Alexandre <alexandre.benoit@live.com>
2019-02-28 20:31:19 -0500

    Updated neural-network for inputs

--- PythonPlays/PythonPlays.py
+++ PythonPlays/PythonPlays.py
@@ -6,16 +6,17 @@ import time
 import random
 import keyboard
 import keyInputs
+import numpy
 import imageProcessing as imgProc
 import grabScreen
 from textAnalyzer import TextAnalyzer
 import tensorflowNN
-import smashMeleeActions
+import smashMeleeInputs
 import tensorflow as tf
 from windowPositioning import openWindow, positionWindow
 
 
-WINDOW_X = 1                                # Default image position for a window perfectly in top left corner
+WINDOW_X = 0                                # Default image position for a window perfectly in top left corner
 WINDOW_Y = 38                               # Default image position for a window perfectly in top left corner
 WINDOW_WIDTH = 1280                         # Modify these values for a window snapped in top left corner
 WINDOW_HEIGHT = 720                         # Modify these values for a window snapped in top left corner
@@ -24,18 +25,20 @@ DIGIT_WIDTH = 45
 DIGIT_HEIGHT = 55
 
 def start_playing():
-    functionList = dir(smashMeleeActions)[8:]
+    inputArray = numpy.zeros(smashMeleeInputs.getSmashMeleeInputs())
+    oldInputArray = inputArray
     #load the digit recognition learning
     digitAnalzer = TextAnalyzer()
     
 
-    model = tensorflowNN.create_model((WINDOW_HEIGHT - WINDOW_Y, WINDOW_WIDTH - WINDOW_X), len(functionList))
+    decisionModel = tensorflowNN.create_model((WINDOW_HEIGHT - WINDOW_Y, WINDOW_WIDTH - WINDOW_X), len(inputArray))
     i = 1
     screen = None
     last_time = time.time()
 
     while True:
         if keyboard.is_pressed("q"):
+            #TODO: add dolphin termination
             cv2.destroyAllWindows()
             break
 
@@ -106,20 +109,34 @@ def start_playing():
         
        
 
-        cv2.imshow("window", screen) # Window showing what is captured
-        cv2.waitKey(1)
+        #cv2.imshow("window", screen) # Window showing what is captured
+        #cv2.waitKey(1)
 
         # Decision making goes here
-        predictions = model.predict(np.array([screen]))
-        try:
-            getattr(smashMeleeActions, functionList[random.randint(0, len(functionList) - 1)])()
-        except:
-            print("Action failed")
-
+        predictions = decisionModel.predict(np.array([screen]))[0]
+        updateKeys(inputArray, oldInputArray, predictions)
         print('loop took {} seconds'.format(time.time()-last_time))
         last_time = time.time()
         
     print("done")
+
+def updateKeys(inputArray, oldInputArray, predictions):
+
+    for index, value in np.ndenumerate(predictions):
+        print(value)
+        if (value > 0.95):
+            inputArray[index] = 1
+        else:
+            inputArray[index] = 0
+
+    for index, value in np.ndenumerate(inputArray):
+        if (value != oldInputArray[index]):
+            #release or press corresponding key
+            if (value == 1):
+                smashMeleeInputs.pressKey(index)
+            elif (value == 0):
+                smashMeleeInputs.releaseKey(index)
+    oldInputArray = inputArray
 
 def main():
     print("starting")
@@ -128,9 +145,9 @@ def main():
         time.sleep(1)
 
 
-    openWindow()
-    positionWindow()
-    #start_playing()
+    #openWindow()
+    #positionWindow()
+    start_playing()
 
 if __name__ == '__main__':
     main()
