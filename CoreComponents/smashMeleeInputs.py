c7ee8c41a743a243bdfd3eda47d377ae71f07158
Alexandre <alexandre.benoit@live.com>
2019-03-07 20:52:23 -0500

    Reduced number of inputs

--- CoreComponents/smashMeleeInputs.py
+++ CoreComponents/smashMeleeInputs.py
@@ -4,9 +4,9 @@ from keyInputs import X, Z, C, S, RETURN, ARROW_UP, ARROW_DOWN, ARROW_LEFT, ARRO
 inputArray = [X, Z, C, S, ARROW_UP, ARROW_DOWN, ARROW_LEFT, ARROW_RIGHT, I, K, J, L, Q, W, T, G, F, H]
 
 def getSmashMeleeInputs():
-    #a,b,x,y,z, start, up, down, left, right, cup, cdown, cleft, cright, L, R, dup, ddown, dleft, dright
-    #x,z,c,s,d,return,up,down,left,right,i,k,j,l,q,w,t,g,f,h
-    return len(inputArray)
+    #a, b, x, y, z, up, down, left, right, cup, cdown, cleft, cright, L, R
+    #x, z, c, s, d, up, down, left, right, i,   k,     j,     l,      q, w
+    return inputArray
 
 def pressKey(index):
     KIpressKey(inputArray[index])
