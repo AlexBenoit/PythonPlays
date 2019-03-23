c4efbd9a8722e465a86678e1e110f7d676a0a3f9
Marc-Antoine Roberge <marc-antoine.roberge.2@ens.etsmtl.ca>
2019-03-23 15:23:06 -0400

    SupervisedTraining

--- SupervisedTrainer/SupervisedTrainer.py
+++ SupervisedTrainer/SupervisedTrainer.py
@@ -1,6 +1,8 @@
 
 #External imports
 import numpy as np
+import os, os.path
+
 
 #Internal imports
 import grabScreen
@@ -10,13 +12,27 @@ from tensorflowNN import DQNSolver
 from globalConstants import WINDOW_X, WINDOW_Y, WINDOW_WIDTH, WINDOW_HEIGHT, BORDER_LEFT, \
 BORDER_RIGHT, BORDER_TOP, BORDER_BOTTOM, MODEL_PATH, MODEL_WEIGHTS_PATH
 
+# Ask start index
+index_string = raw_input("What index should we start training ? ")
+index = int(index_string)
 
-#import data
-screen_data = grabScreen.grab_screen_GRAY(region=(WINDOW_X, WINDOW_Y, WINDOW_WIDTH, WINDOW_HEIGHT)) #TODO: update to data from import           
-input_data = np.zeros(18) #TODO: update to data from import
+
+files_in_directory = [name for name in os.listdir('./input') if os.path.isfile(name)]
 
 dqn_solver = DQNSolver((WINDOW_HEIGHT - WINDOW_Y, WINDOW_WIDTH - WINDOW_X))
 
-dqn_solver.fit(np.array([screen_data]), np.array([input_data]))
+
+for index_file in range(0, index):
+    del files_in_directory[index_file]
+
+for file in files_in_directory:
+    print("Training started for : " + file)
+    data = np.load('./input/' + file)
+    for captured_data in data:
+        screen_data = captured_data[0]
+        input_data = captured_data[1]
+        dqn_solver.fit(np.array([screen_data]), np.array([input_data]))
+
+
 dqn_solver.save_weights(MODEL_WEIGHTS_PATH)
 dqn_solver.save_model(MODEL_PATH)
