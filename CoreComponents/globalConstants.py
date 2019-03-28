0e55e48df15451df5c63f2c9fbd056262b5b3a20
Alexandre <alexandre.benoit@live.com>
2019-03-28 18:28:39 -0400

    Updated pixel values

--- CoreComponents/globalConstants.py
+++ CoreComponents/globalConstants.py
@@ -1,7 +1,7 @@
 
-WINDOW_X = 0                                # Default image position for a window perfectly in top left corner
+WINDOW_X = 320                             # Default image position for a window perfectly in top left corner
 WINDOW_Y = 0                               # Default image position for a window perfectly in top left corner
-WINDOW_WIDTH = 1280                         # Modify these values for a window snapped in top left corner
+WINDOW_WIDTH = 960                         # Modify these values for a window snapped in top left corner
 WINDOW_HEIGHT = 720  
 BORDER_LEFT = 1
 BORDER_RIGHT = 1
